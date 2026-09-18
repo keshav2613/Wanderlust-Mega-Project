088c0b513bb7813e42231bed3fb4121f69ef58f8
keshav2613 <76985675+keshav2613@users.noreply.github.com>
2026-09-18 13:00:24 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,18 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
+  type        = string
   default     = "us-east-2"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
+  type        = string
   default     = "ami-085f9c64a9b75eed5"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
+  type        = string
   default     = "t3.medium"
 }
 
@@ -18,6 +21,7 @@ variable "public_key_path" {
   type        = string
   default     = "~/.ssh/id_rsa.pub"
 }
+
 variable "allowed_ssh_cidr" {
   description = "CIDR block allowed to access the EC2 instance via SSH"
   type        = string
